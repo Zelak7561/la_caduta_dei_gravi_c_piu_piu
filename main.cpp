74503695ce8f5b19eca97b0bcd632151bd3ae8b8
Zelak7561 <bs0057082@gmail.com>
2025-03-11 19:57:13 +0100

    esercizio finito

--- main.cpp
+++ main.cpp
@@ -10,26 +10,26 @@ int main() {
     double h = 0.0;
     double d = 0.0;
     double altezza_s = 0.0;
+    double altezza_c = 0.0;
 
     cout << "*** Caduta dei gravi ***" << endl;
+
     cout << "Inserire il delta s:" << endl;
     cin >> d;
 
     cout << "Inserire l'altezza: " << endl;
     cin >> h;
+    altezza_c = h;
+
 
     double i = 0.0;
-
     cout << "+--t--+---altezza dal suolo----+" << endl;
-    while (h > 0) {
+    while (altezza_c > 0) {
         altezza_s = h - (0.5) * g * pow(i, 2);
+        altezza_c = altezza_s;
 
         cout << left << setw(2) << "|" << fixed << setprecision(2) << i << "|" << right << setw(25) << fixed <<
                             setprecision(10) << altezza_s << "|" << endl;
-
-        if (altezza_s < 0 || altezza_s == 0) {
-            break;
-        }
 
         i = i + d;
     }
