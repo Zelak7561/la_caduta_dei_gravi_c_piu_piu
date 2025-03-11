96e463759ef85e7cc30e621f0b0885c9b1ba1946
Zelak7561 <bs0057082@gmail.com>
2025-03-11 19:50:50 +0100

    esercizio finito

--- main.cpp
+++ main.cpp
@@ -7,24 +7,32 @@ using namespace std;
 const double g = 9.81;
 
 int main() {
+    double h = 0.0;
+    double d = 0.0;
+    double altezza_s = 0.0;
 
-    const double h = 56.70;
-    int d = 0;
-
-    double altezza_finale;
-//3,39
-
-    cout << "Inserire i secondi totali" << endl;
+    cout << "*** Caduta dei gravi ***" << endl;
+    cout << "Inserire il delta s:" << endl;
     cin >> d;
 
-    cout << fixed << setprecision(2);
-    for (int i = 0 ; i <= d; i++) {
-        altezza_finale = (-0.5) * g * pow(i , 2);
-        cout << "t: " << i << " | " << "L'altezza finale e per ogni secondo: " <<  altezza_finale << endl;
+    cout << "Inserire l'altezza: " << endl;
+    cin >> h;
+
+    double i = 0.0;
+
+    cout << "+--t--+---altezza dal suolo----+" << endl;
+    while (h > 0) {
+        altezza_s = h - (0.5) * g * pow(i, 2);
+
+        cout << left << setw(2) << "|" << fixed << setprecision(2) << i << "|" << right << setw(25) << fixed <<
+                            setprecision(10) << altezza_s << "|" << endl;
+
+        if (altezza_s < 0 || altezza_s == 0) {
+            break;
+        }
+
+        i = i + d;
     }
-
-
-
 
 
     return 0;
